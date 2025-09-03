0b9e8e97418b54d57c052060c365525e588fbaaf
Waleed Afridi <patlu7150@gmail.com>
2025-09-03 23:40:44 +0800

    Update settings.py

--- customer_segmentation/settings.py
+++ customer_segmentation/settings.py
@@ -125,3 +125,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://customer-segmentation-ai-app-production.up.railway.app"
+]
