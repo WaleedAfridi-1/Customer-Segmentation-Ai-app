6ec420b53a2813133875b5356c24a914d5a1f299
Waleed Afridi <patlu7150@gmail.com>
2025-09-04 00:22:16 +0800

    Update settings.py

--- customer_segmentation/settings.py
+++ customer_segmentation/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'segmentation/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 
 # Default primary key field type
